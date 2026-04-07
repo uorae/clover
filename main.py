b1ee267df91cdc415101788246769ebb0cbd90ac
Rachel G <rachelg2273@gmail.com>
2026-04-07 16:25:28 +1000

    feature: added camera access

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import os
+import numpy as np
+import cv2 as cv
 from ultralytics import YOLO
+ 
+model = YOLO("best.pt")
+try:
+  cap = cv.VideoCapture(0)
+except:
+  print("Cannot open camera")
+  exit()
 
-# Load a model
-model = YOLO("yolo11n.pt")  # build a new model from YAML
+while True:
+  # Capture frame-by-frame
+  ret, frame = cap.read()
 
-# Train the model
-results = model.train(data="data.yaml", epochs=100, imgsz=640)
+  # if frame is read correctly ret is True
+  if not ret:
+    print("Can't receive frame (stream end?). Exiting ...")
+    break
+  # Our operations on the frame come here
+  results = model(source=frame, show=True, stream=True)
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
